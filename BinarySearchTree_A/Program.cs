ff72252a338059d885fbc76b103bade4e56c6886
FadliStev <wiranto.akbar.ft21@mail.umy.ac.id>
2022-12-28 17:08:41 +0700

    add new method

--- BinarySearchTree_A/Program.cs
+++ BinarySearchTree_A/Program.cs
@@ -9,8 +9,16 @@ namespace BinarySearchTree_A
     class Node
     {
         public string info;
-        public string leftchild;
-        public string rightchild;
+        public Node leftchild;
+        public Node rightchild;
+
+        //Constructor for the Node Class
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
     }
     class Program
     {
